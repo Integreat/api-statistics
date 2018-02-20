e2440292276bef3ba8715af7730fbeb770c7689a
Sven Seeberg <sseebergelverfeldt@suse.com>
2018-02-20 20:05:46 +0100

    Improve manual eval script

--- linecount.sh
+++ linecount.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# Param1: log file, Param2: city name
 LANGS="/en/ /de/ /ar/";
 MONTHS="Jan Feb";
 for MONTH in $MONTHS; do
     for LANG in $LANGS; do
         for i in 0{1..9} {10..31} ; do
-            echo "$LANG; $i/$MONTH; $(cat access.log | grep "$i/$MONTH/2018" | grep $LANG | wc -l)";
+            echo "$LANG; $i/$MONTH; $(cat $1 | grep "$i/$MONTH/2018" | grep $LANG | grep $2 | grep "modified_content/pages" | wc -l)";
         done;
     done;
 done
