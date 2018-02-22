241b56de288454c1a3adf492bb1be43a5749a35d
Sven Seeberg <sseebergelverfeldt@suse.com>
2018-02-22 08:58:36 +0100

    Add help output for linecount.sh

--- linecount.sh
+++ linecount.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # Param1: log file, Param2: city name
-LANGS="/en/ /de/ /ar/";
-MONTHS="Jan Feb";
+LANGS="/en/ /de/ /ar/ /fr/ /es/ /fa/ /am/ /ru/";
+MONTHS="Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec";
+
+if [[ -z $1 || $1 == "--help" || $1 == "-h" ]]; then
+	echo "Usage: linecount.sh [ACCESS.LOG] [CITY]"
+	echo ""
+	echo "You can set environment variables LANGS or MONTHS."
+	echo "Default values:"
+	echo "LANGS=$LANGS"
+	echo "MONTHS=$MONTHS"
+	exit
+fi
+
 for MONTH in $MONTHS; do
     for LANG in $LANGS; do
         for i in 0{1..9} {10..31} ; do
